7c5eeb6108abc494685a9fc6a5586a15fd34b159
Martiño <martinegreirabuja@gmail.com>
2019-03-12 21:25:47 +0100

    Day 03/12/2019

--- ejercicios funciones/app.js
+++ ejercicios funciones/app.js
@@ -2,7 +2,7 @@
  * 1, 2, 3 Go!
  */
 
-// "use strict"; //uso stricto del programa, recomendable poner siempre
+"use strict"; //uso stricto del programa, recomendable poner siempre
 
 // console.log("Bienvenido");
 // alert("Alerta de seguridad");
