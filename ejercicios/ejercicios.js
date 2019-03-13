71fb65e629fd402d479fa77be95c2519c3962321
Martiño <martinegreirabuja@gmail.com>
2019-03-13 20:07:21 +0100

    Day 03/13/2019

---
+++ ejercicios/ejercicios.js
@@ -0,0 +1,129 @@
+//Mecanografia
+
+//If
+
+if (1<2){
+    if(2<3){
+        if(3=4){
+            if(4<3) {
+                if (4 != 3) {
+                    if(2>4){
+                        if(5=3){
+                            if(4=5){
+                                if(2>4) {
+                                    if(10<60) {
+                                        if (40 != 20){
+                                            if (2 < 4) {
+                                                if (6<1) {
+                                                    if (6==7) {
+                                                        if("string" === string){
+                                                            if(string != 4){
+                                                                if("string" == 3){
+                                                                    if(4 = 2){
+                                                                        if(2<4){
+                                                                            if (1023<2){
+                                                                                if("string" == 012){
+                                                                                    if("string" == 34) {
+                                                                                        if(30<12){
+                                                                                            if(30<56){
+                                                                                                if (10 <3445){
+                                                                                                    if(23<334){
+                                                                                                        if("strong" == "string"){
+                                                                                                            if("strong" == "agua"){
+                                                                                                                if(1 = 3){
+                                                                                                                    if (23 <45){
+                                                                                                                        if(34 < 324) {
+                                                                                                                            if(e434 = 423) {
+                                                                                                                                if(4534 < 6456){
+                                                                                                                                    if ("string" == string){
+                                                                                                                                        if(string != 2){
+                                                                                                                                            if(string ==strong){
+                                                                                                                                                if(string < string){
+                                                                                                                                                    if(12 < 45){
+                                                                                                                                                        if (232 < 343){
+                                                                                                                                                            if (string == strong){
+                                                                                                                                                                if(string == 32){
+                                                                                                                                                                    if ("strong" == 32){
+                                                                                                                                                                        if(string == "strong"){
+                                                                                                                                                                            if(string == 243){
+                                                                                                                                                                                if (123423 <4354){
+                                                                                                                                                                                    if (242 < 3553){
+                                                                                                                                                                                        if(23243<545){
+                                                                                                                                                                                            if("string" == "string"){
+                                                                                                                                                                                                if(string != string){
+                                                                                                                                                                                                    if(10 < 20){
+                                                                                                                                                                                                        if(r35e < 4534) {
+                                                                                                                                                                                                            if(100 < 243){
+                                                                                                                                                                                                                if (100 < 34){
+                                                                                                                                                                                                                    if (20 < 30){
+                                                                                                                                                                                                                        if(100 < 200){
+                                                                                                                                                                                                                            if (100 == 23){
+                                                                                                                                                                                                                                if(100 != 33){
+                                                                                                                                                                                                                                    if(100 < 100){
+                                                                                                                                                                                                                                        if(100 != 100){
+                                                                                                                                                                                                                                            if(100 != 100){
+
+                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                            }
+                                                                                                                                                                                                        }
+                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                }
+                                                                                                                                                                                            }
+                                                                                                                                                                                        }
+                                                                                                                                                                                        
+                                                                                                                                                                                    }
+                                                                                                                                                                                }
+                                                                                                                                                                            }
+                                                                                                                                                                        }
+                                                                                                                                                                    }
+                                                                                                                                                                }
+                                                                                                                                                            }
+                                                                                                                                                        }
+                                                                                                                                                        
+                                                                                                                                                    }
+                                                                                                                                                }
+                                                                                                                                            }
+                                                                                                                                        }
+                                                                                                                                    }
+                                                                                                                                }
+                                                                                                                                
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ nuevo dia/apuntes.js
@@ -0,0 +1,25 @@
+// Las funciones expresiva no puede llevar un nombre porque ya lo lleva let,const
+// Llamar un funcion nombre de funcion()
+// Typoff te dice que tipo de palabra,numero o objeto 
+// Tyoff de NaN da Number
+
+// function numer(num){
+//     if(num % 2 == 0){
+//         return true;
+
+//     }
+//     return false;
+//     function test(resultado){
+//     return test(num)
+//     console.log(resultado);
+// }
+// }
+
+
+
+
+
+//arrow fuction = Es otra funcion expresiva
+//Esta quita la palabra funcion y ponemos un arrow delante =>
+//Si es de una linea esta funcion retorna automatica si es en una sola linea
+//el this te dice donde estas mediante un console.log, los arrow funtion hacen que sean globales
