87aaed59239d6e73db7caed967b62b1856fbe244
Martiño <martinegreirabuja@gmail.com>
2019-03-14 16:17:01 +0100

    Mecanografia para recordar ifs,fuctions,fors

---
+++ ejercicios-mecanografia/funciones.js
@@ -0,0 +1,45 @@
+function amigos(colegas){
+    for(let felicidad; felicidad == amigos();felicidad++){
+        if(felicidad == 100) {
+            for(let comida; comida < amigos; felicidad++){
+                function trabajo(programacion){
+                    for(let esfuerzo = 100; esfuerzo == trabajo(10);esfuerzo++){
+                        if(trabajo == comida){
+                            function fiesta(domingos){
+                                for(let beber = 0; beber != fiesta(0); beber--){
+                                    if(beber < felicidad){
+                                        function agua(sal){
+                                            for(let rio = 20; rio == sal(12); sal++){
+                                                if(rio == agua){
+                                                    function colonia(olor){
+                                                        for(let manzana = 100; colonia() != manzana; manzana++){
+                                                            function comer (kas){
+                                                                for(let hamburgesa = 1; comer() = hamburgesa; felicidad++) {
+                                                                    if(agua < sal){
+                                                                        agua = 10 
+                                                                        for(let piscina = 1; agua = piscina; agua++){
+                                                                            
+                                                                        }
+                                                                    }
+                                                                
+                                                                }
+                                                                
+                                                            }
+                                                }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                            }
+                            
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
